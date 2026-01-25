5ef07d80f042535d5c230a2dc4c24b3643bb0243
Tek <tekbista375@gmail.com>
2026-01-25 15:54:59 -0500

    updated the security config to allow traffic to all the end points

--- src/main/java/com/bista/user_profile_service/security/SecurityConfig.java
+++ src/main/java/com/bista/user_profile_service/security/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/actuator/**", "/health", "/user/*").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
 
